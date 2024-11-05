a0d9cb6167e0a58ff365abbe8ff647c7412ed260
Ali <alimanghat99@gmail.com>
2024-11-05 13:51:39 -0800

    Added strings sort function and the unit testing for it

--- integerationProject_excercise.py
+++ integerationProject_excercise.py
@@ -8,3 +8,12 @@ def bubbleSort(nums):
                 nums[j] = nums[j + 1]
                 nums[j + 1] = temp
     return nums
+
+def sortStrings(array):
+    for i in range(len(array)):
+        for j in range(len(array) - i - 1):
+            if array[j] > array[j + 1]:
+                temp = array[j]
+                array[j] = array[j + 1]
+                array[j + 1] = temp
+    return array
--- test_sort_functions.py
+++ test_sort_functions.py
@@ -9,6 +9,14 @@ class TestSortingFunctions(unittest.TestCase):
         self.assertEqual(bubbleSort([1]), [1])
         self.assertEqual(bubbleSort([1, 2, 3]), [1, 2, 3])
         self.assertEqual(bubbleSort([4, 5, 5, 4]), [4, 4, 5, 5])
+    
+    def test_sort_strings(self):
+        self.assertEqual(sortStrings(["apple", "orange", "banana", "grape", "cherry"]),
+                         ["apple", "banana", "cherry", "grape", "orange"])
+        self.assertEqual(sortStrings(["z", "a"]), ["a", "z"])
+        self.assertEqual(sortStrings(["single"]), ["single"])
+        self.assertEqual(sortStrings([]), [])
+        self.assertEqual(sortStrings(["pear", "apple", "pear"]), ["apple", "pear", "pear"])
 
 if __name__ == "__main__":
     unittest.main()
